41973f0fc4262787755e8790a65b06f93276f8bb
Cody kelderman <codykelderman@gmail.com>
2023-06-18 11:06:22 -0700

    display x on click

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ function gameBoard() {
     const gameContainer = document.querySelector('.gameContainer')
     const renderBoard = () => {
         for (let i = 0; i < board.length; i++) {
-            const gameDiv = document.createElement('div')
-            let getId = ''
+            const gameDiv = document.createElement('div');
             gameDiv.setAttribute('id', i)
             gameDiv.classList.add('gameStyle')
             gameContainer.appendChild(gameDiv);
             gameDiv.addEventListener('click', e => {
                 if(board[e.target.id] === '') {
                     board[e.target.id] = 'x';
+                    e.target.textContent = 'x';
                 } else if(board[e.target.id] === 'x') {
                     alert('Nope!')
                 };
@@ -32,4 +32,9 @@ function gameBoard() {
 //get position
 
 gameBoard();
+
+//set player
+const setPlayer = (name, player) => {
+    return {name, player};
+}
 
