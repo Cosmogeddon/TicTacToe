011ae7024ce5611480e11ec24187a4a55c97507c
Cody kelderman <codykelderman@gmail.com>
2023-07-11 16:56:33 -0700

    update gameBoard

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
 (function() {
     const resetButton = document.getElementById('resetButton');
     const gameContainer = document.querySelector('#gameContainer');
-    let gameBoard = ['', '', '',
-                    '', '', '',
+
+    let gameBoard = ['x', '', '',
+                    '', 'o', '',
                     '', '', ''];
 
     const Player = (sign) => {
         return {sign};
     };
 
-    (function gameController() {
+    function playerMaker() {
         let playerX = Player('X');
         let playerO = Player('O');
         let round = false;
 
         return {playerX, playerO, round}
-    })();
+    };
+
+
+
     function renderGameboard() {
         for (let i = 0; i < gameBoard.length; i++) {
             let gameDiv = document.createElement('div');
             gameDiv.className = 'gameDivs';
+            gameDiv.innerText = gameBoard[i];
+            gameDiv.id = [i];
             gameContainer.appendChild(gameDiv);
         };
 
     };
+
     renderGameboard();
+
+    function getID() {
+        document.addEventListener('click',(event)=>{
+            let cellID = event.target.getAttribute('id');
+            if(playerMaker.round === false) {
+                gameBoard.splice(cellID, 1, 'X')
+                playerMaker.round = true;
+            } else {
+                gameBoard.splice(cellID, 1, 'O')
+                playerMaker.round = false;
+            };
+            console.log(gameBoard);
+
+    })};
+    getID();
+
+
 })();
