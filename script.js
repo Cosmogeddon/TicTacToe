4f5afca3f677f113abe03e2250f56181d4be95e9
Cody kelderman <codykelderman@gmail.com>
2023-07-16 19:32:25 -0700

    update winning message

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ const cell5 = document.getElementById('5')
 const cell6 = document.getElementById('6')
 const cell7 = document.getElementById('7')
 const cell8 = document.getElementById('8')
-const cell9= document.getElementById('9')
+const cell9 = document.getElementById('9')
+const namen = document.getElementById('namen')
+const nameValue = document.getElementById('nameValue')
 const results = document.getElementById('results')
 const cells = document.querySelectorAll('.gameDiv')
 const resetButton = document.getElementById('resetButton')
 
-const playerFactory = (sign) => {
-  return {sign};
+const playerFactory = (sign, name) => {
+  return {sign, name};
 };
 
-const player1 = playerFactory('x');
-const player2 = playerFactory('o');
+const player1 = playerFactory('X', '');
+const player2 = playerFactory('O', '');
 let turn = true;
 let sign = '';
 let winnerDeclared = false;
@@ -35,6 +37,27 @@ cells.forEach( function(element){
         console.log(gameBoard)
     });
 });
+
+function getName() {
+    tempName = player2.name;
+    if (player1.name == '') {
+        player1.name = nameValue.value;
+        nameValue.value = '';
+    } else if (player1.name !== '' && player2.name == '') {
+        player2.name = nameValue.value;
+        nameValue.value = '';
+    } else if (player1.name !== '' && player2.name !== ''){
+        nameValue.value = '';
+        results.innerHTML = 'There are already two players.'
+        setTimeout(clearBox, 3000)
+    }
+}
+
+function clearBox() {
+    results.innerHTML = '';
+}
+
+namen.addEventListener('click', getName);
 
 resetButton.addEventListener('click', resetTable)
 
@@ -100,29 +123,59 @@ function renderGameboard(e) {
 function checkWinner() {
     if (winnerDeclared == true) {
         return;
-    } else if (cell0.innerHTML !== ''  && cell1.innerHTML == cell2.innerHTML && cell0.innerHTML == cell1.innerHTML) {
-    results.innerText = `${sign}'s win!`;
+    } else if (cell0.innerHTML !== ''  && cell0.innerHTML == 'X' && cell1.innerHTML == cell2.innerHTML && cell0.innerHTML == cell1.innerHTML) {
+    results.innerText = `${player1.name} wins!`;
     winnerDeclared = true;
-    } else if (cell3.innerHTML !== ''  && cell4.innerHTML == cell3.innerHTML && cell5.innerHTML == cell3.innerHTML) {
-    results.innerText = `${sign}'s win!`;
+    } else if (cell3.innerHTML !== ''  && cell3.innerHTML == 'X' && cell4.innerHTML == cell3.innerHTML && cell5.innerHTML == cell3.innerHTML) {
+    results.innerText = `${player1.name} wins!`;
     winnerDeclared = true;
-    } else if (cell6.innerHTML !== ''  && cell7.innerHTML == cell6.innerHTML && cell8.innerHTML == cell7.innerHTML) {
-    results.innerText = `${sign}'s win!`;
+    } else if (cell6.innerHTML !== ''  && cell6.innerHTML == 'X' && cell7.innerHTML == cell6.innerHTML && cell8.innerHTML == cell7.innerHTML) {
+    results.innerText = `${player1.name} wins!`;
     winnerDeclared = true;
-    } else if (cell0.innerHTML !== ''  && cell3.innerHTML == cell0.innerHTML && cell6.innerHTML == cell3.innerHTML) {
-    results.innerText = `${sign}'s win!`;
+    } else if (cell0.innerHTML !== ''  && cell0.innerHTML == 'X' && cell3.innerHTML == cell0.innerHTML && cell6.innerHTML == cell3.innerHTML) {
+    results.innerText = `${player1.name}'s win!`;
     winnerDeclared = true;
-    } else if (cell1.innerHTML !== ''  && cell4.innerHTML == cell1.innerHTML && cell7.innerHTML == cell4.innerHTML) {
-    results.innerText = `${sign}'s win!`;
+    } else if (cell1.innerHTML !== ''  && cell1.innerHTML == 'X' && cell4.innerHTML == cell1.innerHTML && cell7.innerHTML == cell4.innerHTML) {
+    results.innerText = `${player1.name} wins!`;
     winnerDeclared = true;
-    } else if (cell2.innerHTML !== ''  && cell5.innerHTML == cell2.innerHTML && cell8.innerHTML == cell5.innerHTML) {
-    results.innerText = `${sign}'s win!`;
+    } else if (cell2.innerHTML !== ''  && cell2.innerHTML == 'X' && cell5.innerHTML == cell2.innerHTML && cell8.innerHTML == cell5.innerHTML) {
+    results.innerText = `${player1.name} wins!`;
     winnerDeclared = true;
-    } else if (cell0.innerHTML !== ''  && cell4.innerHTML == cell0.innerHTML && cell0.innerHTML == cell8.innerHTML) {
-    results.innerText = `${sign}'s win!`;
+    } else if (cell0.innerHTML !== ''  && cell0.innerHTML == 'X' && cell4.innerHTML == cell0.innerHTML && cell0.innerHTML == cell8.innerHTML) {
+    results.innerText = `${player1.name} wins!`;
     winnerDeclared = true;
-    } else if (cell2.innerHTML !== ''  && cell4.innerHTML == cell2.innerHTML && cell2.innerHTML == cell6.innerHTML) {
-    results.innerText = `${sign}'s win!`;
+    } else if (cell2.innerHTML !== ''  && cell2.innerHTML == 'X' && cell4.innerHTML == cell2.innerHTML && cell2.innerHTML == cell6.innerHTML) {
+    results.innerText = `${player1.name} wins!`;
+    winnerDeclared = true;
+    }  else if (cell0.innerHTML !== ''  && cell0.innerHTML == 'X' && cell1.innerHTML == cell2.innerHTML && cell0.innerHTML == cell1.innerHTML) {
+    results.innerText = `${player1.name.name} wins!`;
+    winnerDeclared = true;
+    } else if (cell0.innerHTML !== ''  && cell0.innerHTML == 'O' && cell1.innerHTML == cell2.innerHTML && cell0.innerHTML == cell1.innerHTML) {
+    results.innerText = `${player2.name} wins!`;
+    winnerDeclared = true;
+    } else if (cell3.innerHTML !== ''  && cell3.innerHTML == 'O' && cell4.innerHTML == cell3.innerHTML && cell5.innerHTML == cell3.innerHTML) {
+    results.innerText = `${player2.name} wins!`;
+    winnerDeclared = true;
+    } else if (cell6.innerHTML !== ''  && cell6.innerHTML == 'O' && cell7.innerHTML == cell6.innerHTML && cell8.innerHTML == cell7.innerHTML) {
+    results.innerText = `${player2.name} wins!`;
+    winnerDeclared = true;
+    } else if (cell0.innerHTML !== ''  && cell0.innerHTML == 'O' && cell3.innerHTML == cell0.innerHTML && cell6.innerHTML == cell3.innerHTML) {
+    results.innerText = `${player2.name} wins!`;
+    winnerDeclared = true;
+    } else if (cell1.innerHTML !== ''  && cell1.innerHTML == 'O' && cell4.innerHTML == cell1.innerHTML && cell7.innerHTML == cell4.innerHTML) {
+    results.innerText = `${player2.name} wins!`;
+    winnerDeclared = true;
+    } else if (cell2.innerHTML !== ''  && cell2.innerHTML == 'O' && cell5.innerHTML == cell2.innerHTML && cell8.innerHTML == cell5.innerHTML) {
+    results.innerText = `${player2.name} wins!`;
+    winnerDeclared = true;
+    } else if (cell0.innerHTML !== ''  && cell0.innerHTML == 'O' && cell4.innerHTML == cell0.innerHTML && cell0.innerHTML == cell8.innerHTML) {
+    results.innerText = `${player2.name} wins!`;
+    winnerDeclared = true;
+    } else if (cell2.innerHTML !== ''  && cell2.innerHTML == 'O' && cell4.innerHTML == cell2.innerHTML && cell2.innerHTML == cell6.innerHTML) {
+    results.innerText = `${player2.name} wins!`;
+    winnerDeclared = true;
+    }  else if (cell0.innerHTML !== ''  && cell0.innerHTML == 'O' && cell1.innerHTML == cell2.innerHTML && cell0.innerHTML == cell1.innerHTML) {
+    results.innerText = `${player2.name} wins!`;
     winnerDeclared = true;
     } else if(cell0.innerHTML !== '' && cell1.innerHTML !== '' && cell2.innerHTML !== '' && cell3.innerHTML !== '' && cell4.innerHTML !== '' && cell5.innerHTML !== '' && cell6.innerHTML !== '' && cell7.innerHTML !== '' && cell8.innerHTML !== ''){
         results.innerText = 'Tie';
